8c3f573a5de868f904d4e460e5ff7e7b85641cea
lliam mcbean <lliam.mcbean@gmail.com>
2021-02-23 16:05:09 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const app = require('express')()
+const csv = require('csv-parser')
+const PORT = 8080
+
+app.listen(
+    PORT,
+    () => console.log('hello')
+)
+
+app.get('/', (req, res) => {
+    res.status(200).send(results)
+})
+
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('bitcoin_csv.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
