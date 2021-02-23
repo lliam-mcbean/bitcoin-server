db3dc6c925dacbd21f20f400e25614dcbde0639f
lliam mcbean <lliam.mcbean@gmail.com>
2021-02-23 16:55:08 -0700

    jdss

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const app = require('express')()
-const PORT = 8080
+const PORT = process.env.PORT || 3000
 const importdata = require('./bitcoin.json')
 
 app.listen(
     PORT,
     () => console.log('hello')
 )
-
-app.get('/', (req, res) => {
-    res.send('hello')
-})
 
 app.get('/public', (req, res) => {
     res.send(importdata)
