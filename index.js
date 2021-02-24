a4a38d2b85aa045e223b471eecd603ef862fb955
lliam mcbean <lliam.mcbean@gmail.com>
2021-02-23 17:11:26 -0700

    hdbjsd

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const app = require('express')()
 const csv = require('csv-parser')
 const PORT = process.env.PORT || 3000
 
+const fs = require('fs')
+const results = [];
+
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+
 app.listen(
     PORT,
     () => console.log('hello')
@@ -10,10 +17,3 @@ app.listen(
 app.get('/', (req, res) => {
     res.send(results)
 })
-
-const fs = require('fs')
-const results = [];
-
-fs.createReadStream('data.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
